e262c879708b0a149cbc94429bcd10add1852791
Jatin Singh <singhjatin1812@gmail.com>
2026-02-24 12:20:41 +0530

    Initial Commit

---
+++ papaplayer/papaplayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  papaplayerApp.swift
+//  papaplayer
+//
+//  Created by Jatin Singh on 24/02/26.
+//
+
+import SwiftUI
+
+@main
+struct papaplayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ papaplayerTests/papaplayerTests.swift
@@ -0,0 +1,17 @@
+//
+//  papaplayerTests.swift
+//  papaplayerTests
+//
+//  Created by Jatin Singh on 24/02/26.
+//
+
+import Testing
+@testable import papaplayer
+
+struct papaplayerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
